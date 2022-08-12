733df4c33fe6f65e1d341915c3cf315e0376b31f
Bailey Cage <bailey.cage@gmail.com>
2022-08-11 20:15:00 -0400

    add send data functionality

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Add your code here
+const postUserData = (userName, userEmail) => {
+    return {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            "name": userName,
+            "email": userEmail
+        }),
+    }
+};
+
+function submitData (userName, userEmail) {
+    fetch("http://localhost:3000/users", postUserData(userName, userEmail))
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+    });
+}
+
+submitData("test", "test2");
+
+
+
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   });
